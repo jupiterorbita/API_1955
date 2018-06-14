9367e387a6eaeae30fb644da9bafdd1aa01fd4b6
jupterorbita <john.williams@outlook.com>
2018-06-14 11:47:50 -0700

    still fixing

--- server.js
+++ server.js
@@ -1,80 +1,66 @@
-var path = require('path');
+var path = require("path");
 
-var express = require('express');
+var express = require("express");
 var app = express();
 
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 //use for json
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, './client/views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "./client/views"));
+app.set("view engine", "ejs");
 
 const server = app.listen(5000);
-console.log('SERVER ===========> listening on port: 5000');
-
+console.log("SERVER ===========> listening on port: 5000");
 
 //mongoose
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/1955_api_db");
 mongoose.Promise = global.Promise;
 
-
-
 //schema model
-const PersonSchema = new mongoose.Schema({
+const PersonSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 20
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20
     }
-}, {
+  },
+  {
     timestamps: true
-});
-mongoose.model('Person', PersonSchema);
+  }
+);
+mongoose.model("Person", PersonSchema);
 
 var Person = mongoose.model("Person");
 
-
-
 //routes
-app.get('/', function (req, res) {
-    Person.find({}, function (err, person) {
-        if (err) {
-            console.log("Returned error", err);
-            // respond with JSON
-            res.json({
-                message: "Error",
-                error: err
-            })
-        } else {
-            // respond with JSON
-            res.json({
-                message: "Success",
-                data: person
-            })
-        }
-    })
-})
+app.get("/", function(req, res) {
+  Person.find({}, function(err, ppl) {
+    if (err) {
+      console.log("Returned error", err);
+      res.json({ message: "Error", error: err });
+    } else {
+      res.json({ message: "Success", data: ppl });
+    }
+  });
+});
 
-app.get('/new/:name', function (req, res) {
-    console.log("POST DATA", req.body);
-    var new_name = new Person({
-        name: req.params.name
-    });
-    new_name.save(function (err) {
-        if (err) {
-            console.log("something went wrong inserting");
-            res.json({
-                message: "ERROR",
-                error: err
-            });
-        } else {
-            console.log("added person!");
-            res.json({
-                message: "success"
-            })
-        }
-    })
-})
+app.get("/new/:name", function(req, res) {
+//   console.log("POST DATA", req.body);
+  var new_name = new Person({name: req.params.name});
+  new_name.save(function(err) {
+    if (err) {
+      console.log("something went wrong inserting");
+      res.json({
+        message: "ERROR",
+        error: err
+      });
+    } else {
+      console.log("added person!");
+      res.json({message: "success"});
+    }
+  });
+});
